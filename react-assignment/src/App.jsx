9a1ae8aeadbb5a4f64957ecec75558152e1b6c02
Vinich <omugavinich@gmail.com>
2025-06-26 22:09:50 +0300

    initial2

--- react-assignment/src/App.jsx
+++ react-assignment/src/App.jsx
@@ -9,14 +9,7 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -26,12 +19,12 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the React logos to learn more
       </p>
-      <h1>Makau did this</h1>
-
+      <h1>Vinich did this</h1>
+      </div>
     </>
   )
 }
 
-export default App
+
